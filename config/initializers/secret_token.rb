a000ef572c5b6c7bc55c2607e4df614b1a080939
Tyson Trotter <ttrotter@leafsoftwaresolutions.com>
2013-07-08 13:27:48 -0400

    Rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RfePartsApp::Application.config.secret_token = '52543eac2875f16be8e5f97747ae5d79403b6698a8c65d676f45ba027edbc598f655f229d74610c26d973ddb6f9556d5f1148e6ed1a739c73fcbebd5a4c5209d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RfePartsApp::Application.config.session_store :cookie_store, key: '_rfe_parts_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RfePartsApp::Application.config.session_store :active_record_store

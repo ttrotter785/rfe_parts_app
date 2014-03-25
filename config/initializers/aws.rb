afa5530cf5ce976a6204df9efe1770bf80625c2d
ttrotter785 <ttrotter25@hotmail.com>
2014-03-25 13:27:15 -0400

    removing secret

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,4 +1,4 @@
-AWS_SECRET_KEY_ID='AKIAJO5ONK5UQSNMG6MQ'
-AWS_SECRET_KEY   ='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'
+AWS_SECRET_KEY_ID=''
+AWS_SECRET_KEY   =''
 AWS.config({:access_key_id => AWS_SECRET_KEY_ID, :secret_access_key => AWS_SECRET_KEY})
 AWS::Record.domain_prefix = "rfe_parts_app-#{Rails.env}-"
